0840655ba1accc3068c718c6f78ec5a1127f8b6e
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2024-12-24 19:04:50 +0530

    add:) Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import http from 'http';
+import app from './app.js';
+
+const port = process.env.PORT || 3001;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+ console.log(`Server is running on port ${port}`);
+})
