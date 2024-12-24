50fb6915e75bf972be70a679370b6306c0d70434
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2024-12-24 19:46:03 +0530

    create:) models

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
-// import connect from './db/db.js';
-// import userRoutes from './routes/user.routes.js';
-// import projectRoutes from './routes/project.routes.js';
-// import aiRoutes from './routes/ai.routes.js';
+import connect from './db/db.js';
+import userRoutes from './routes/user.routes.js';
+import projectRoutes from './routes/project.routes.js';
+import aiRoutes from './routes/ai.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-// connect();
+connect();
 
 
 const app = express();
@@ -17,12 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// app.use('/users', userRoutes);
-// app.use('/projects', projectRoutes);
-// app.use("/ai", aiRoutes)
+app.use('/users', userRoutes);
+app.use('/projects', projectRoutes);
+app.use("/ai", aiRoutes)
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World Jagan!');
 });
 
 export default app; 
